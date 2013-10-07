245de865f26391eb3ffb468112322080164590e0
Thibault Duplessis <thibault.duplessis@gmail.com>
2013-10-07 09:51:54 +0200

    server is working

--- project/Build.scala
+++ project/Build.scala
@@ -1,16 +1,18 @@
-import sbt._
-import Keys._
+import sbt._, Keys._
 
 object ScalariverProject extends Build {
 
-  val scalariver = (Project("scalariver", file(".")) settings (
+  val scalariver = Project("scalariver", file(".")) settings (
     organization := "com.github.ornicar",
     name := "scalariver",
     version := "0.1",
     scalaVersion := "2.10.3",
-    libraryDependencies ++= dependencies,
     resourceDirectories in Compile := List(),
-    scalacOptions := Seq("-deprecation", "-unchecked", "-feature", "-language:_")) 
-
-  val compilerOptions = Seq("-deprecation", "-unchecked", "-feature", "-language:_")
+    libraryDependencies := Seq(
+      "org.gnieh" %% "tiscaf" % "0.8-SNAPSHOT",
+      "org.scalariform" %% "scalariform" % "0.1.4"),
+      resolvers := Seq(
+        "sonatype" at "http://oss.sonatype.org/content/repositories/releases",
+        "sonatype snapshots" at "http://oss.sonatype.org/content/repositories/snapshots"),
+        scalacOptions := Seq("-deprecation", "-unchecked", "-feature", "-language:_"))
 }
---
+++ src/main/scala/Scalariver.scala
@@ -0,0 +1,67 @@
+package com.github.ornicar.scalariver
+
+import tiscaf._
+
+object ScalariverServer extends HServer with App {
+
+  def apps = Seq(ScalariformApp, StaticApp)
+  def ports = Set(8080)
+  override protected def name = "scalariver"
+  override protected def maxPostDataLength = 1024 * 8
+  // do not start the stop thread
+  override protected def startStopListener {}
+
+  start
+  println("press enter to stop...")
+  Console.readLine
+  stop
+}
+
+/** The application that serves the pages */
+object ScalariformApp extends HApp {
+
+  override def keepAlive = false
+  override def chunked = false
+  override def buffered = false
+  override def gzip = false
+  override def tracking = HTracking.NotAllowed
+
+  def resolve(req: HReqData): Option[HLet] = 
+    if (req.method == HReqType.PostData) Some(ScalariformLet) else None
+}
+
+/** Serves the current server time */
+object ScalariformLet extends HSimpleLet {
+
+  import scalariform.formatter.ScalaFormatter
+  import scala.util.{ Try, Success, Failure }
+
+  def act(talk: HTalk) {
+    val source = talk.req.softParam("source")
+    Try(ScalaFormatter format source) match {
+      case Success(x) ⇒ talk
+        .setContentType("text/plain")
+        .setContentLength(x.length)
+        .write(x)
+      case Failure(e) ⇒ talk
+        .setStatus(HStatus.BadRequest)
+        .setContentLength(e.getMessage.length)
+        .write(e.getMessage)
+    }
+  }
+
+}
+
+/** Simply servers the resource from the classpath */
+object StaticApp extends HApp {
+
+  override def buffered: Boolean = true // ResourceLet needs buffered or chunked be set
+
+  def resolve(req: HReqData) = Some(StaticLet) // generates 404 if resource not found
+}
+
+object StaticLet extends let.ResourceLet {
+  protected def dirRoot = ""
+  override protected def uriRoot = ""
+  override protected def indexes = List("index.html")
+}
