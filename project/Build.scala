5026b01304d2ad8cffa1097864ae2bb1d5a1d7e7
Thibault Duplessis <thibault.duplessis@gmail.com>
2013-11-18 10:50:14 +0100

    publish using one-jar

--- project/Build.scala
+++ project/Build.scala
@@ -2,7 +2,7 @@ import sbt._, Keys._
 
 object ScalariverProject extends Build {
 
-  val scalariver = Project("scalariver", file(".")) settings (
+  val scalariver = Project("scalariver", file(".")).settings(
     organization := "com.github.ornicar",
     name := "scalariver",
     version := "1.0",
@@ -11,8 +11,9 @@ object ScalariverProject extends Build {
     libraryDependencies := Seq(
       "org.gnieh" %% "tiscaf" % "0.8",
       "org.scalariform" %% "scalariform" % "0.1.4"),
-      resolvers := Seq(
-        "sonatype" at "http://oss.sonatype.org/content/repositories/releases",
-        "sonatype snapshots" at "http://oss.sonatype.org/content/repositories/snapshots"),
-        scalacOptions := Seq("-deprecation", "-unchecked", "-feature", "-language:_"))
+    resolvers := Seq(
+      "sonatype" at "http://oss.sonatype.org/content/repositories/releases",
+      "sonatype snapshots" at "http://oss.sonatype.org/content/repositories/snapshots"),
+    scalacOptions := Seq("-deprecation", "-unchecked", "-feature", "-language:_")
+  ).settings(com.github.retronym.SbtOneJar.oneJarSettings: _*)
 }
